f1cb46c583936258db362d4b11c91de89c30974d
Mikeps2 <mikeps.dev@gmail.com>
2024-09-24 23:30:16 -0300

    css

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
+let cardsArray = [];
+let difficulty = 'Easy';
+let rankingScore = 0;
 
+
+function toggleDifficulty() {
+    let gameDifficultyOption = document.getElementById('game-difficulty-option');
+    
+    if (gameDifficultyOption.textContent == 'Difficulty: Easy') {
+        difficulty = 'Hard';
+        gameDifficultyOption.textContent = `Difficulty: ${difficulty}`;
+    } else {
+        difficulty = 'Easy';
+        gameDifficultyOption.textContent = `Difficulty: ${difficulty}`;
+    }
+}
+
+
+function displayCards(cardsNumber, difficulty) {
+    let memoryGameInterface = `
+        <div>
+            <ul>
+                // mostrar cards de acordo com a dificuldade
+                <li>card</li>
+            </ul>
+        </div>
+    `
+    
+    // substitui o conteúdo do menu pelos cards
+    let menu = document.getElementById('menu');
+    menu.replaceChildren(memoryGameInterface)
+    
+
+    // mudar a classe
+    // adicionar a div com as cartas
+}
+
+
+function playGame(difficulty) {
+    const playGameButton = document.getElementById('play-game-button');
+
+    // verificar dificuldade
+    if (difficulty == 'Easy') {
+        // displayCards(16);
+    } else {
+        // displayCards(32);
+    }
+}
+
+
+function shuffleCards() {
+
+}
+
+
+function playAgain() {
+
+}
